daf9b9f33de3c36e2ef2ef5b99902e9dc6b83d93
sacpatil1995 <sachin@rtosoftware.com.au>
2023-10-07 17:34:17 +0100

    fixed alignment related issues and removed animcss

--- script.js
+++ script.js
@@ -6,39 +6,13 @@ const box = document.getElementById("animated-box");
 const animateButton = document.getElementById("animate-button");
 
 speakButton.addEventListener("click", () => {
-    const text = textToSpeak.value;
-    const utterance = new SpeechSynthesisUtterance(text);
+  const text = textToSpeak.value;
+  const utterance = new SpeechSynthesisUtterance(text);
 
-    synth.speak(utterance);
-    output.innerText = "Speaking...";
-   
-    utterance.onend = () => {
-        output.innerText = "Speech Complete";
-    };
+  synth.speak(utterance);
+  output.innerText = "Speaking...";
+
+  utterance.onend = () => {
+    output.innerText = "Speech Complete";
+  };
 });
-
-
-let voices = [];
-
-
-speechSynthesis.addEventListener('voiceschanged', event => {
-    voices = speechSynthesis.getVoices();
-    // if (!CONFIG.DEFAULT) {
-    //     textToSpeak.voice = voices.find(voice => voice.name === CONFIG.VOICE);
-    // }
-});
-
-textToSpeak.onend = function(event) {
-    robot.classList.remove('robot_speaking');
-}
-
-robot.addEventListener('click', event => {
-    if (speechSynthesis.speaking) {
-        robot.classList.remove('robot_speaking');
-        speechSynthesis.cancel();
-    } else {
-        robot.classList.add('robot_speaking');
-        speechSynthesis.speak(textToSpeak);
-    }
-});
-
