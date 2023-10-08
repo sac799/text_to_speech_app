38161d10b265c1c1e5d2ad01bea98db84b97dff9
sacpatil1995 <sachin@rtosoftware.com.au>
2023-10-08 21:07:16 +0100

    added clear text button

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 const textToSpeak = document.getElementById("textToSpeak");
 const speakButton = document.getElementById("speakButton");
+const clearButton = document.getElementById("clearButton");
 const output = document.getElementById("output");
 const synth = window.speechSynthesis;
 const box = document.getElementById("animated-box");
 const animateButton = document.getElementById("animate-button");
+clearButton.disabled = true;
 
-speakButton.addEventListener("click", () => {
-  const text = textToSpeak.value;
-  const utterance = new SpeechSynthesisUtterance(text);
+document.addEventListener("DOMContentLoaded", function () {
+  speakButton.addEventListener("click", () => {
+    const text = textToSpeak.value;
+    const utterance = new SpeechSynthesisUtterance(text);
+    synth.speak(utterance);
+    output.innerText = "Speaking...";
+    utterance.onend = () => {
+      output.innerText = "Speech Complete";
+    };
+  });
 
-  synth.speak(utterance);
-  output.innerText = "Speaking...";
+  const clearButton = document.getElementById("clearButton");
+  const textToSpeak = document.getElementById("textToSpeak");
 
-  utterance.onend = () => {
-    output.innerText = "Speech Complete";
-  };
+  function toggleButtons() {
+    if (textToSpeak.value.trim() === "") {
+      clearButton.disabled = true;
+      speakButton.disabled = true;
+    } else {
+      clearButton.disabled = false;
+      speakButton.disabled = false;
+    }
+  }
+
+  textToSpeak.addEventListener("input", toggleButtons);
+  clearButton.addEventListener("click", function () {
+    textToSpeak.value = ""; 
+
+    toggleButtons(); 
+  });
+
+  toggleButtons();
 });
